94c7f8d1a5f4130bc20ee7461441b19152256dad
Toru Hotta <toru1055h@gmail.com>
2017-10-20 22:54:45 +0900

    Debug and change views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_brands
+    Brand.joins(:items).group("brands.id").having("count(items.id) > ?", 0)
+  end
+
+  def default_image
+    "https://placehold.it/170x170"
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :brand
+
+  def wrapped_image
+    wrap_image = "https://placehold.it/170x170"
+    if main_image != nil && main_image.length > 0
+      wrap_image = main_image
+    end
+    return wrap_image
+  end
 end
